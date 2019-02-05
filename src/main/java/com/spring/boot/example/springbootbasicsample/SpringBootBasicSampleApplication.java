1b480d9587bdda68e84537a458df6fbcb2477724
Fernando Silva <Fernando.Silva@vistajet.com>
2019-02-05 04:53:29 -0200

    feature: start project with spring boot initializer
    This was done adding no additional spring boot modules. Simple config

---
+++ src/main/java/com/spring/boot/example/springbootbasicsample/SpringBootBasicSampleApplication.java
@@ -0,0 +1,14 @@
+package com.spring.boot.example.springbootbasicsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBasicSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBasicSampleApplication.class, args);
+    }
+
+}
+
